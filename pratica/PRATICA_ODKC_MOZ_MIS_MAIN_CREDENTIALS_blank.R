7820195833d781c8f2e086327316ee22927c6acf
eds <eleanore@trophealth.com>
2023-10-06 15:41:42 +0200

    Added credentials script for main survey (and added complete version to gitignore)

---
+++ pratica/PRATICA_ODKC_MOZ_MIS_MAIN_CREDENTIALS_blank.R
@@ -0,0 +1,10 @@
+## Setting ODK Central Access Information for Main MIS Data
+
+Sys.setenv(ODKC_SVC="https://estatisticas.ins.gov.mz/v1/projects/10/forms/PRACTICA_ENTREVISTA.svc")
+Sys.setenv(ODKC_UN="username") ## ADD YOUR INFO HERE
+Sys.setenv(ODKC_PW="password") ## ADD YOUR INFO HERE
+# Sys.setenv(ODKC_PP="...")
+# Sys.setenv(ODKC_TEST_URL="...")
+# Sys.setenv(ODKC_TEST_UN="...")
+# Sys.setenv(ODKC_TEST_PW="...")
+# Sys.setenv(ODKC_TEST_PID=2)
