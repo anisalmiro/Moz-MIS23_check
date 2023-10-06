7eb916d1a1a52c125372661f247ca0998bb20112
eds <eleanore@trophealth.com>
2023-10-06 15:38:13 +0200

    Added blank credentials for HH listing

---
+++ pratica/PRATICA_ODKC_HH_LISTING_CREDENTIALS_blank.R
@@ -0,0 +1,11 @@
+## Setting ODK Central Access Information 
+
+Sys.setenv(ODKC_SVC="https://estatisticas.ins.gov.mz/v1/projects/10/forms/PRACTICA_LISTAGEM.svc")
+Sys.setenv(ODKC_UN="login") ## ADD YOUR INFO HERE
+Sys.setenv(ODKC_PW="password") ## ADD YOUR INFO HERE
+# Sys.setenv(ODKC_PP="...")
+# Sys.setenv(ODKC_TEST_URL="...")
+# Sys.setenv(ODKC_TEST_UN="...")
+# Sys.setenv(ODKC_TEST_PW="...")
+# Sys.setenv(ODKC_TEST_PID=2)
+
